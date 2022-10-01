540b2a90b4c7ddd5ccf8ce0b5bd2a9e857bc3e49
Динара <zaripovadin@yandex.ru>
2022-10-01 11:46:24 +0300

    test

--- app/src/main/java/com/example/androidlab_2022/MainActivity.kt
+++ app/src/main/java/com/example/androidlab_2022/MainActivity.kt
@@ -2,9 +2,17 @@ package com.example.androidlab_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.androidlab_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var binding: ActivityMainBinding? = null
+
+    private val score = 0
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
