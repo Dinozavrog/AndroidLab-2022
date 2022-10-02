9100c95ae249fc0f5966d0f9f0f730b6153e849e
Динара <zaripovadin@yandex.ru>
2022-10-02 22:33:12 +0300

    test

--- app/src/main/java/com/example/androidlab_2022/MainActivity.kt
+++ app/src/main/java/com/example/androidlab_2022/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.androidlab_2022
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat.startActivity
 import com.example.androidlab_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +17,57 @@ class MainActivity : AppCompatActivity() {
             setContentView(it.root)
         }
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding?.run {
+            btnGoogle.setOnClickListener {
+                sendImplictIntentIntent()
+            }
+            btnPhone.setOnClickListener {
+                sendContact()
+            }
+            btnEmail.setOnClickListener {
+                sendEmail()
+            }
+        }
+
+    }
+
+    private fun sendImplictIntentIntent() {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = Uri.parse("geo:47.6,-122.3?z=11")
+        }
+        val chooserIntent = Intent.createChooser(
+            intent,
+            "hi "
+        )
+        if (chooserIntent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    private fun sendContact() {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$89172872567")
+        }
+        val chooserIntent = Intent.createChooser(
+            intent,
+            "hi "
+        )
+        if (chooserIntent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    private fun sendEmail() {
+        val recipients = arrayOf("zaripovadin@yandex.ru")
+        val subject = "Dinozavr"
+        val content = "Hi, dinozavr!"
+        var intentEmail: Intent = Intent(Intent.ACTION_SEND, Uri.parse("mailto:"));
+        intentEmail.putExtra(Intent.EXTRA_EMAIL, recipients);
+        intentEmail.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intentEmail.putExtra(Intent.EXTRA_TEXT, content);
+        intentEmail.setType("text/plain");
+
+        startActivity(Intent.createChooser(intentEmail, "Choose an email client from..."));
     }
 }
---
+++ app/src/main/java/com/example/androidlab_2022/SecondActivity.kt
@@ -0,0 +1,36 @@
+package com.example.androidlab_2022
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.androidlab_2022.databinding.ActivityMainBinding
+import com.example.androidlab_2022.databinding.ActivitySecondBinding
+
+
+class SecondActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySecondBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySecondBinding.inflate(layoutInflater).also {
+            setContentView(it.root)
+        }
+
+
+        intent?.also{
+            if(it.action == Intent.ACTION_SEND && it.type=="text/plain"){
+
+                binding.tvFirst.text = intent.getStringExtra(Intent.EXTRA_TEXT)
+
+            }
+        }
+
+
+
+    }
+
+
+}
